94acbbc67b83659d50933b44e8188de9436af652
Vijyesh1709 <vijramrekha@gmail.com>
2020-02-07 16:09:32 +0000

    urls working

--- rango/views.py
+++ rango/views.py
@@ -5,3 +5,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 def index (request):
     return HttpResponse("Rango says hey there partner! ")
+
+def about (request):
+    return HttpResponse("Rango says this is about! ")
